198d838ae85a711749a11c1d0cb0bd8826190b2c
Syrows <clement.gho@gmail.com>
2021-12-11 22:26:16 +0100

    Adding menu

--- main.c
+++ main.c
@@ -4,6 +4,14 @@
 #include <string.h>
 #include "arbre.h"
 
+unsigned int input_number(char* msg);
+
+void clear_input();
+
+void compute_input(char choice);
+
+void display_menu();
+
 int main() {
 
     T_Tree tree = NULL;
@@ -11,30 +19,68 @@ int main() {
     tree = add_node(tree, "occurrence");
     tree = add_node(tree, "binaire");
     tree = add_node(tree, "binary");
-    // tree = add_node(tree, "recherche");
-    // tree = add_node(tree, "test");
-    // tree = add_node(tree, "plein");
+    tree = add_node(tree, "recherche");
+    tree = add_node(tree, "test");
+    tree = add_node(tree, "plein");
     tree = add_node(tree, "arbre");
     tree = add_node(tree, "binaire");
-    // tree = add_node(tree, "plein");
-    // tree = add_node(tree, "occurrence");
-
-    // display_tree(tree, '\0');
-
-    printf("%d\n", is_equilibrate(tree));
-
-    tree = remove_occurrence(tree, "occurrence", 2);
-    tree = remove_occurrence(tree, "plein", 2);
-    tree = remove_occurrence(tree, "arbre", 2);
-    tree = remove_occurrence(tree, "binaire", 2);
-    tree = remove_occurrence(tree, "binary", 2);
-    tree = remove_occurrence(tree, "test", 2);
-    tree = remove_occurrence(tree, "recherche", 2);
-    tree = remove_occurrence(tree, "recherche", 2);
-
-    // tree = remove_occurrence(tree, "binaire", 1);
+    tree = add_node(tree, "plein");
+    tree = add_node(tree, "occurrence");
 
     display_tree(tree, '\0');
 
+    char c;
+
+    do {
+
+        display_menu();
+
+        c = getchar();
+        clear_input();
+
+        compute_input(c);
+
+    } while (c != '9');
+    
     return 0;
+}
+
+unsigned int input_number(char* msg) {
+
+    unsigned int number;
+
+    printf("%s\n", msg);
+
+    scanf("%u", &number);
+    clear_input();
+
+    return number;
+}
+
+void clear_input() {
+
+    int c;
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+}
+
+void display_menu() {
+
+    printf("\n-------- Menu de selection --------\n");
+    printf("Entrer un choix parmi les suivants :\n");
+    printf("1. Creer un nouvel arbre.\n");
+    printf("2. Afficher un arbre.\n");
+    printf("3. Ajouter un mot dans un arbre.\n");
+    printf("4. Retirer un mot d'un arbre.\n");
+    printf("5. Verifier si un arbre est parfait.\n");
+    printf("6. Verifier si un arbre est equilibre.\n");
+    printf("7. Transformer un arbre en un lexique (liste lineairement chainee) et afficher le lexique.\n");
+    printf("8. Tester la similarite de deux textes.\n");
+    printf("9. Quitter.\n");
+    printf("-----------------------------------\n");
+}
+
+void compute_input(char choice) {
+
 }
